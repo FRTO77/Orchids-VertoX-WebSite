dacee7604304d3ab6db2a5132ad23bfb153f7f0f
FRTO77 <144280787+FRTO77@users.noreply.github.com>
2026-01-19 00:04:34 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,4 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-const loaderPath = require.resolve('orchids-visual-edits/loader.js');
 
 const nextConfig: NextConfig = {
   images: {
@@ -15,21 +13,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [loaderPath]
-      }
-    }
-  }
+  
 }
 
 export default nextConfig;
-// Orchids restart: 1768757072146
